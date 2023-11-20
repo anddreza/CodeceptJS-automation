3f04861de2cbc61cc0ef82307333c6ee52b85bd3
Andreza <dedezasilva@live.com>
2023-11-20 09:47:03 -0300

    feat: aula 'Aula 4: Campos texto'

--- projects/codecept-express/e2e/tasks_test.js
+++ projects/codecept-express/e2e/tasks_test.js
@@ -1,11 +1,14 @@
 Feature('tasks');
 
 Scenario.only('deve poder cadastrar uma nova tarefa',  ({ I }) => {
-	
+	const taskName = 'Ler um livro de JS'
 	I.amOnPage('/')
-	I.fillField('input[placeholder="Add a new Task"]', 'Ler um livro de JS')
+
+	I.fillField('input[placeholder$=Task]', taskName)
+	//input[placeholder="Add a new Task"]
 	I.click('Create')
-	I.wait(5)
+	I.see(taskName, '.task-item')
+	//.task-item
 
 
 });
