327de9d142752f606145c6c35a9f80c5394ecb08
Andreza <dedezasilva@live.com>
2023-11-20 15:26:43 -0300

    feat: inserção de novo cenário, gerando testes independentes

--- projects/codecept-express/e2e/tasks_test.js
+++ projects/codecept-express/e2e/tasks_test.js
@@ -1,14 +1,44 @@
+const { faker } = require('@faker-js/faker');
 Feature('tasks');
 
-Scenario.only('deve poder cadastrar uma nova tarefa',  ({ I }) => {
-	const taskName = 'Ler um livro de JS'
+Scenario('deve poder cadastrar uma nova tarefa',  ({ I }) => {
+
+	const taskName = 'Fazer compras'
+
+	I.sendDeleteRequest('/helper/tasks/' + taskName)
+	//I.seeResponseCodeIsSucessful()
 	I.amOnPage('/')
 
-	I.fillField('input[placeholder$=Task]', taskName)
+	I.fillField('input[class*=NewTask]', taskName)
 	//input[placeholder="Add a new Task"]
 	I.click('Create')
 	I.see(taskName, '.task-item')
 	//.task-item
 
+
+});
+
+
+Scenario.only('Não deve cadastrar tarefas com nome duplicado', ({I})=> {
+	
+	//Dado que eu tenho uma nova tarefa
+	const task = {
+		name: 'Pagar a fatura do cartão',
+		is_done: false
+	} 
+
+	I.sendDeleteRequest('/helper/tasks/' + task.name)
+	I.seeResponseCodeIsSuccessful()
+
+	//Mas essa tarefa já foi cadastrada anteriormente e eu não lembrava disso
+	I.sendPostRequest('/tasks', task)
+	I.seeResponseCodeIsSuccessful()
+
+	//Quando tento cadastrar essa tarefa novamente
+	I.amOnPage('/')
+	I.fillField('input[class*=NewTask]', task.name)
+	I.click('Create')
+		//Então deve ver uma mensagem de duplicidade
+	I.see('Task already exists!', '.swal2-html-container')
 
 });
